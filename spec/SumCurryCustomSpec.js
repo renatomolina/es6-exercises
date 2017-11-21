da17944917dcde8c931f46afb644a7272ebca521
Renato Toth <renatomolinat@gmail.com>
2017-11-20 23:26:14 -0800

    creating my own curry custom function

--- spec/SumCurryCustomSpec.js
+++ spec/SumCurryCustomSpec.js
@@ -1,12 +1,20 @@
-//var _ = require('lodash');
-import _ from 'lodash';
-
 describe('Sum function', function () {
   it('let my own curry func to return the sum', function () {
-    expect(_.curry(sum)(10)(5)).toEqual(15);
+    expect(curryCustom(sum)(10)(1)(4)).toEqual(15);
   });
 });
 
-let sum = (x, y) => x + y;
+let sum = (x, y, z) => x + y + z;
 
+let curryCustom = function(func) {
+  return function curried(...args) {
+    if(args.length >= func.length){
+      return func(...args);
+    } else {
+      return function(param) {
+        return curried(...[...args, param]);
+      }
+    }
+  }
+}
 
